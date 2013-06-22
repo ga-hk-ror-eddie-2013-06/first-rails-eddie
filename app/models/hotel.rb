5c530ed9940afe552cd7cab754b1afc079afae21
3dd13 <tatonlto@gmail.com>
2013-06-22 12:10:43 +0800

    upload whole rails app

---
+++ app/models/hotel.rb
@@ -0,0 +1,3 @@
+class Hotel < ActiveRecord::Base
+  attr_accessible :name, :price
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '2f411cdd18917efe4bcac09c243cbc328a8a2fc9e58a5f11f1cb1432e1afe31765de74bb695292c20f2d11e1d8b45b301831b1b4e3eba2bf05b3b518311eb3e9'
